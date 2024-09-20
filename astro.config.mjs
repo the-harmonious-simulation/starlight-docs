dc1043010a6400dda2fd0647a2fde65db59cd494
Thomas Harmon <thomas.alan.harmon@gmail.com>
2024-09-19 19:11:31 -0700

    fix: Set site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://the-harmonious-simulation.github.io',
+	base: '/starlight-docs',
 	integrations: [
 		starlight({
 			title: 'Saas Template Docs',
